1b3358daa89e6cabbe0ce7df84a2204e3f09c413
kamendd1 <kamen_damianov@abv.bg>
2016-02-19 18:01:09 +0200

    Added Charts SVG

---
+++ charts.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Kamen on 19.2.2016 г..
+ */
+$("#htmlChart").drawDoughnutChart([
+    {
+        title: "Very good level",
+        value : 90,
+        color: "#00386d"
+    },
+    {
+        title: "You can always get better",
+        value:  10,
+        color: "#63ceff"
+    }
+]);
+
+$("#photoshopChart").drawDoughnutChart([
+    {
+        title: "Good working level",
+        value : 80,
+        color: "#00386d"
+    },
+    {
+        title: "I can still learn",
+        value:  20,
+        color: "#63ceff"
+    }
+]);
+
+$("#jsChart").drawDoughnutChart([
+    {
+        title: "Working level",
+        value : 60,
+        color: "#00386d"
+    },
+    {
+        title: "I can learn a lot more",
+        value:  40,
+        color: "#63ceff"
+    }
+]);
+
+$("#angChart").drawDoughnutChart([
+    {
+        title: "I Have Some Experience",
+        value : 50,
+        color: "#00386d"
+    },
+    {
+        title: "Need to learn more",
+        value:  50,
+        color: "#63ceff"
+    }
+]);
+
+$("#wpChart").drawDoughnutChart([
+    {
+        title: "I Have Some Experience",
+        value : 50,
+        color: "#00386d"
+    },
+    {
+        title: "Need to learn more",
+        value:  50,
+        color: "#63ceff"
+    }
+]);
